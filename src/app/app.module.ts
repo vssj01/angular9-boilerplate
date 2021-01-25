6244c675044f66efd41f0d3cd49b53edd56f16fe
Sai Jyothi <vssj01@gmail.com>
2021-01-25 08:02:43 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import {
+  AccordionModule,
+  AlertModule, // Foundation Callouts
+  ButtonsModule,
+  CarouselModule, // Foundation Orbit
+  CollapseModule,
+  BsDatepickerModule,
+  BsDropdownModule, // Foundation Dropdown Menus and Dropdown Panes
+  ModalModule, // Foundation Reveal
+  OffcanvasModule,
+  PaginationModule,
+  ProgressbarModule,
+  RatingModule,
+  SortableModule,
+  TabsModule,
+  TimepickerModule,
+  TooltipModule,
+  TypeaheadModule,
+} from 'ngx-foundation';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TabsComponent],
+  imports: [BrowserModule, AppRoutingModule, TabsModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/index.ts
@@ -0,0 +1,5 @@
+export * from './buttons';
+export * from './datatable';
+export * from './indicators';
+export * from './layout';
+export * from './popups';
